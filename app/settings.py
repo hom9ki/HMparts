a09b7300eead652038c5cf91761fd1899a0be145
hom9ki <156588482+hom9ki97@users.noreply.github.com>
2025-09-24 23:55:00 +0300

    Create app feedback and models, project setup.

--- app/settings.py
+++ app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'product',
     'garage',
+    'feedback',
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('product.urls')),
     path('account/', include('users.urls')),
     path('garage/', include('garage.urls')),
+    path('feedback/', include('feedback.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
