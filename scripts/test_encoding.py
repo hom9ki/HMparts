2caeda31f098ac65e2e594113d95be27a7e0cefc
hom9ki <156588482+hom9ki97@users.noreply.github.com>
2025-11-04 00:13:46 +0300

    Update fixtures

---
+++ scripts/test_encoding.py
@@ -0,0 +1,31 @@
+import os
+import chardet
+
+
+def fix_encoding():
+    try:
+        dump_path = "../fixtures/dump.json"
+
+        if not os.path.exists(dump_path):
+            print("Файл не найден")
+            return False
+        with open(dump_path, 'rb') as f:
+            raw_data = f.read()
+            encoding = chardet.detect(raw_data)['encoding']
+            print(f'Обнаружена кодировка: {encoding}')
+
+        with open(dump_path, 'r', encoding=encoding) as f:
+            content = f.read()
+
+        with open(dump_path, 'w', encoding='utf-8') as f:
+            f.write(content)
+
+        print("Файл успешно перекодирован")
+        return True
+    except Exception as e:
+        print(f"Ошибка при перекодировании: {e}")
+        return False
+
+
+if __name__ == '__main__':
+    fix_encoding()
