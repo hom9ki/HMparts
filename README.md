a8c2abdd4b3624ee00417ef8b89e41fabd062546
Sergey <kpytooooo@gmail.com>
2025-11-08 01:48:57 +0300

    Update serializers.py

--- README.md
+++ README.md
@@ -3,27 +3,28 @@
 Automotive parts website
 
 # Создание виртуального окружения
-
+```
 python -m venv .venv
-
+```
 # Активация (Linux/Mac)
-
-source venv/bin/activate
-
+```
+source .venv/bin/activate
+```
 # Активация (Windows)
-
+```
 venv\Scripts\activate
-
+```
 # Установка зависимостей в виртуальном окружении
-
+```
 pip install -r requirements.txt
-
+```
 # Инициализация БД
 
-1. Необходимо изменить данные в DATABASE app/settings.py на свои
-2. Необходимо изменить данные super_db_config scripts/settings.py на свои от пользователя postgres
-3. Запустит скрипт из файла run_db.py
+1. Необходимо изменить данные в `DATABASE` `app/settings.py` на свои
+2. Необходимо изменить данные `super_db_config` `scripts/settings.py` на свои от пользователя postgres
+3. Запустить скрипт из файла `run_db.py`
 
 # Запуск отладочного сервера
-
+```
 python manage.py runserver
+```
--- cart/models.py
+++ cart/models.py
@@ -1,33 +1,58 @@
 from django.db import models
-from users.models import CustomUser
+from django.db.models.functions import Round
+
 from product.models import Product
+from users.models import CustomUser
 
 
-class CartQuerySet(models.QuerySet):
+class CartItemsQuerySet(models.QuerySet):
     def total_price(self):
-        return sum(cart.product_price() for cart in self)
+        return sum(cart_item.price for cart_item in self.annotate_prices())
 
     def total_quantity(self):
-        return sum(cart.quantity for cart in self)
+        return sum(cart_item.quantity for cart_item in self)
+
+    def annotate_prices(self):
+        # оптимальнее считать цены для всего набора сразу, а не по одномму
+        return self.annotate(
+            price=Round(models.F("product__price") * models.F("quantity"), 2)
+        )
 
 
+class CartItemsManager(models.Manager):
+    def get_queryset(self):
+        return CartItemsQuerySet(self.model, using=self._db)
+
+
+# CartItem я бы назвал, у тебя больше на такую логику похоже
 class Cart(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь',
-                             related_name='user_cart')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None, verbose_name='Товар',
-                                related_name='product_cart')
-    quantity = models.SmallIntegerField(default=0, verbose_name='Количество')
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Пользователь",
+        related_name="user_cart",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        default=None,
+        verbose_name="Товар",
+        related_name="product_cart",
+    )
+    quantity = models.SmallIntegerField(default=0, verbose_name="Количество")
     session_key = models.CharField(max_length=255, blank=True, null=True)
-    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    created_timestamp = models.DateTimeField(
+        auto_now_add=True, verbose_name="Время создания"
+    )
 
     class Meta:
-        verbose_name = 'Корзина'
-        verbose_name_plural = 'Корзины'
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
 
-    objects = CartQuerySet().as_manager()
-
-    def product_price(self):
-        return round(self.product.price * self.quantity, 2)
+    objects = CartItemsManager()
 
     def __str__(self):
-        return f'{self.user} - {self.product} - {self.quantity}'
+        # Мы точно не хотим тянуть вызов метода __str__ из модели Продукт
+        return f"{self.user}: {self.product.slug} - {self.quantity} шт."
