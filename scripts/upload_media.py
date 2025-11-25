c013147dbf55d400d0512b793c7a5243b7456477
xomma <hom9ki83@mail.ru>
2025-11-25 17:09:24 +0300

    update func name in upload_media.py

--- scripts/upload_media.py
+++ scripts/upload_media.py
@@ -3,7 +3,7 @@ import yadisk
 from settings import TOKEN, REMOTE_MEDIA_PATH, LOCAL_MEDIA_PATH
 
 
-def upload_recurcive(ya, remote_path, local_path):
+def upload_media(ya, remote_path, local_path):
     for item_name in os.listdir(local_path):
         if item_name == '.gitkeep':
             continue
@@ -15,14 +15,14 @@ def upload_recurcive(ya, remote_path, local_path):
             if not ya.exists(remote_item_path):
                 ya.makedirs(remote_item_path)
 
-            upload_recurcive(ya, remote_item_path, local_item_path)
+            upload_media(ya, remote_item_path, local_item_path)
             print(f'Загружена папка: {item_name}')
         else:
             ya.upload(local_item_path, remote_item_path)
             print(f'Загружен файл: {item_name}')
 
 
-def upload_media():
+def connect_yandex():
     print('Выгрузка медиафайлов на Яндекс.Диск')
     try:
         ya = yadisk.YaDisk(token=TOKEN)
@@ -36,7 +36,7 @@ def upload_media():
             ya.makedirs(REMOTE_MEDIA_PATH)
             print(f' Создана удаленная папка: {REMOTE_MEDIA_PATH}')
 
-        upload_recurcive(ya, REMOTE_MEDIA_PATH, LOCAL_MEDIA_PATH)
+        upload_media(ya, REMOTE_MEDIA_PATH, LOCAL_MEDIA_PATH)
 
         print('Медиафайлы успешно загружены на Яндекс.Диск')
         return True
@@ -47,4 +47,4 @@ def upload_media():
 
 
 if __name__ == '__main__':
-    upload_media()
+    connect_yandex()
